7d3205b1c0423e73d60201df3a9efd0e96a79942
lfurmanchuk <lesyazhuruk@gmail.com>
2022-11-11 02:09:37 +0200

    task-02, 03

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,9 @@
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +12,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const createItemEl = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+
+  return item;
+});
+
+ingredientsEl.append(...createItemEl);
--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,10 @@
+// Напиши скрипт для створення галереї зображень на підставі масиву даних. HTML містить список ul.gallery.
+// <ul class="gallery"></ul>
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +19,24 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryEl = document.querySelector(".gallery");
+
+const imageEl = images
+  .map(image => `<li class="galllery__item"><img class="gallery__img" scr="${image.url}" alt-"${image.alt}"></li>`)
+  .join("");
+
+console.log(imageEl);
+galleryEl.insertAdjacentHTML("afterbegin", imageEl);
+
+const listEl = document.querySelector('.gallery')
+console.log(listEl)
+
+const itemEl = images
+  .map((image) => `<li class="list-item"> <img src='${image.url}' alt='${image.alt}' width=240 class="item-img"> </li>`)
+  .join("");
+console.log(itemEl)
+
+listEl.insertAdjacentHTML('afterbegin', itemEl);
+
+
